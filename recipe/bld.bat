3d12f027c08ec2fae7151cd303e32caa81107a79
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-15 14:10:18 +0200

    Remove the obsolete code

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,4 @@
 set MACHINE="AMD64"
-@REM :: A different SDK is needed when build with VS 2017 and 2015
-@REM :: http://wiki.tcl.tk/54819
-@REM if "%VS_MAJOR%"=="14" (
-@REM   echo "Switching SDK versions"
-@REM   call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
-@REM )
 
 echo puts [info tclversion] | tclsh > tmpfile.tcl
 set /p TCL_VERSION=<tmpfile.tcl
