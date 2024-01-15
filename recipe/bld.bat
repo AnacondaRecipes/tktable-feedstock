5a2fc380ad9a25565c236fae8ebff063165856c1
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-15 13:56:05 +0200

    Replace the tcl versions hardcoding by checking 'put [info tclversion]'

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,22 +1,19 @@
-if "%ARCH%"=="32" (
-   set MACHINE="IX86"
-   :: A different SDK is needed when build with VS 2017 and 2015
-   :: http://wiki.tcl.tk/54819
-   if "%VS_MAJOR%"=="14" (
-    echo "Switching SDK versions"
-    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x86 10.0.15063.0
-   )
-) else (
-  set MACHINE="AMD64"
-  :: A different SDK is needed when build with VS 2017 and 2015
-  :: http://wiki.tcl.tk/54819
-  if "%VS_MAJOR%"=="14" (
-    echo "Switching SDK versions"
-    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
-  )
-)
+@REM set MACHINE="AMD64"
+@REM :: A different SDK is needed when build with VS 2017 and 2015
+@REM :: http://wiki.tcl.tk/54819
+@REM if "%VS_MAJOR%"=="14" (
+@REM   echo "Switching SDK versions"
+@REM   call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
+@REM )
+
+echo puts [info tclversion] | tclsh > tmpfile.tcl
+set /p TCL_VERSION=<tmpfile.tcl
+del tmpfile.tcl
+set TCL_VER=%TCL_VERSION:.=%
+echo %TCL_VERSION%
+echo %TCL_VER%
 
 pushd win
-nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=86 TCL_VERSION=8.6 DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
+nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
 if %ERRORLEVEL% GTR 0 exit 1
 popd
