fc6c3534a979889c6926208516f6526a50ee4662
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-17 15:02:33 +0200

    Fix tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,8 +8,7 @@ echo %TCL_VERSION%
 echo %TCL_VER%
 
 pushd win
-nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE%
-nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% test
 nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
+nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% test
 if %ERRORLEVEL% GTR 0 exit 1
 popd
