a58ca4cbb8c0b09ce15be88dd0af2f1f6c577621
Serhii Kupriienko <skupriienko@anaconda.com>
2024-01-17 14:50:06 +0200

    Add tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,8 @@ echo %TCL_VERSION%
 echo %TCL_VER%
 
 pushd win
+nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE%
+nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% test
 nmake -f makefile.vc libpath32="" CC=%CC% LD=link.exe TCL_VER=%TCL_VER% TCL_VERSION=%TCL_VERSION% DEST_DIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
 if %ERRORLEVEL% GTR 0 exit 1
 popd
